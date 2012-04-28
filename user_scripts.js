2734816bc82dfa14b57f7b187e6bd9dd59528cc1
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-04-28 15:33:41 +0900

    Support pivotal id in commit title

--- user_scripts.js
+++ user_scripts.js
@@ -2,21 +2,34 @@
   const PIVOTAL_STORY_URL = 'https://www.pivotaltracker.com/story/show/'
       , STORY_ID_MATCHER = /\[(?:Finish )?#([0-9]{8})\]/
       , LINK_TEMPLATE = '<a href="'+PIVOTAL_STORY_URL+'%s">%s</a>'
+      , QUERIES = ['.commit .commit-title', '.commit .commit-desc']
 
-  var messages = document.querySelectorAll('.commit .commit-desc');
-  var i, message, text, replaced;
-  for (i = messages.length; i--;) {
-    message = messages.item(i);
-    text = message.innerText;
-
-    replaced = text.replace(STORY_ID_MATCHER, function(matched, id) {
-      var values = [id, matched]
-        , i = 0
-      return LINK_TEMPLATE.replace(/%s/g, function() {
-        return values[i++ % 2];
-      });
-    });
-
-    message.innerHTML = replaced;
-  }
+  QUERIES.forEach(function(query) {
+    var messages = document.querySelectorAll(query);
+    var i, message, text, replaced, anchor;
+    for (i = messages.length; i--;) {
+      message = messages.item(i);
+      text = message.innerHTML;
+      anchor = message.querySelector('a');
+      if (anchor) {
+        replaced = text.replace(STORY_ID_MATCHER, function(matched, id) {
+          var values = [id, matched]
+            , i = 0
+          var linked = LINK_TEMPLATE.replace(/%s/g, function() {
+            return values[i++ % 2];
+          });
+          return '</a>' + linked + '<a href="' + anchor.getAttribute('href') + '">'
+        });
+      } else {
+        replaced = text.replace(STORY_ID_MATCHER, function(matched, id) {
+          var values = [id, matched]
+            , i = 0
+          return LINK_TEMPLATE.replace(/%s/g, function() {
+            return values[i++ % 2];
+          });
+        });
+      }
+      message.innerHTML = replaced;
+    }
+  });
 }();
