dc4e46c1ca48bc557d7a525592c69b54796406b9
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-04-28 14:59:05 +0900

    Support pivotar id in commit description

--- user_scripts.js
+++ user_scripts.js
@@ -1,1 +1,22 @@
-// TODO Start coding!
+!function() {
+  const PIVOTAL_STORY_URL = 'https://www.pivotaltracker.com/story/show/'
+      , STORY_ID_MATCHER = /\[(?:Finish )?#([0-9]{8})\]/
+      , LINK_TEMPLATE = '<a href="'+PIVOTAL_STORY_URL+'%s">%s</a>'
+
+  var messages = document.querySelectorAll('.commit .commit-desc');
+  var i, message, text, replaced;
+  for (i = messages.length; i--;) {
+    message = messages.item(i);
+    text = message.innerText;
+
+    replaced = text.replace(STORY_ID_MATCHER, function(matched, id) {
+      var values = [id, matched]
+        , i = 0
+      return LINK_TEMPLATE.replace(/%s/g, function() {
+        return values[i++ % 2];
+      });
+    });
+
+    message.innerHTML = replaced;
+  }
+}();
