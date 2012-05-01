47c0d5dbe911fb47ad6bbe066bedda7c2bf974c6
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-05-01 13:49:19 +0900

    Add class to link to pivotal
    Use 'issue-link' that is used for github issue.

--- link-to-pivotal.user.js
+++ link-to-pivotal.user.js
@@ -1,7 +1,7 @@
 !function() {
   const PIVOTAL_STORY_URL = 'https://www.pivotaltracker.com/story/show/'
       , STORY_ID_MATCHER = /\[(?:[a-zA-Z]+ )?#([0-9]{8})\]/
-      , LINK_TEMPLATE = '<a href="'+PIVOTAL_STORY_URL+'%s">%s</a>'
+      , LINK_TEMPLATE = '<a href="'+PIVOTAL_STORY_URL+'%s" class="issue-link">%s</a>'
       , QUERIES = ['.commit .commit-title', '.commit .commit-desc']
 
   var linkToPivotal = function(text, processor) {
