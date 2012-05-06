bb0f5934937e1c51c6931e7fdeb9b835c116c01a
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-05-06 20:26:42 +0900

    Bump version to 0.0.2

--- link-to-pivotal.user.js
+++ link-to-pivotal.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Link to Pivotal
-// @version     0.0.1
+// @version     0.0.2
 // @namespace   https://github.com/tricknotes
 // @description Auto link to Pivotal Tracker from Github.
 // @license     MIT License
